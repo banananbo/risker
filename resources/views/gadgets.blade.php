ae93b87cf9b8f39f6d9e2ae5887d53132798a673
obana-takenori <obana-takenori@dmm.com>
2018-01-17 16:38:39 +0900

    session test

--- resources/views/gadgets.blade.php
+++ resources/views/gadgets.blade.php
@@ -1,9 +1,25 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>'; ?>
+<?php
+session_start();
+if (!isset($_SESSION['count'])) {
+    // キー'count'が登録されていなければ、1を設定
+    $_SESSION['count'] = 1;
+} else {
+    //  キー'count'が登録されていれば、その値をインクリメント
+    $_SESSION['count']++;
+}
+
+
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
+
+?>
 <Module>
     <ModulePrefs title=" Sample Game ">
     </ModulePrefs>
     <Content type="html" view="canvas"><![CDATA[
         <div id="sample">Test Game</div>
+        <?php
+          echo $_SESSION['count']."回目の訪問です。";
+        ?>
         <script type="text/javascript">
             function init() {
                 document.getElementById("sample").innerHTML = " Sample Game ";
